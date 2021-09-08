14402450daca1018f69ffd3521af3b7790e3e14d
sw720201@gmail.com <>
2021-09-07 20:28:07 -0500

    versión 1

---
+++ src/suma/Suma.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma;
+
+/**
+ *
+ * @author Gerar
+ */
+public class Suma {
+    private int vUno,vDos,resultado;
+    public Suma(int valorUno,int valorDos){
+        vUno=valorUno;
+        vDos=valorDos;
+       
+        
+    }
+    
+    public void Operacion(){
+        resultado=vUno + vDos;
+    }
+    
+    public void Imprimir(){
+        Operacion();
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+}
